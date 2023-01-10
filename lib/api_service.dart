4e892f6513b552a0269ba7fa97d3cb25487ed6a4
minseo-and <msp05070478@gmail.com>
2023-01-10 13:43:46 +0900

    웹툰 정보 보이기

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_episode_model.dart';
 import 'package:webtoon/models/webtoon_model.dart';
+import 'package:webtoon/models/webtoon_detail_model.dart';
 
 class ApiService {
   static const String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
@@ -16,8 +18,30 @@ class ApiService {
       for (var webtoon in webtoons) {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-      print(webtoonInstances);
       return webtoonInstances;
+    }
+    throw Error();
+  }
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLastestEpisodeId(String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webtoon/api_service.dart';
 import 'package:webtoon/models/webtoon_model.dart';
+import 'package:webtoon/widgets/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -27,14 +28,11 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot){
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index){
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(width: 20,),
+            return Column(
+              children: [
+                const SizedBox(height: 50,),
+                Expanded(child: makeList(snapshot)),
+              ],
             );
           }
           return const Center(
@@ -43,6 +41,20 @@ class HomeScreen extends StatelessWidget {
         },
       ),
     );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+            scrollDirection: Axis.horizontal,
+            itemCount: snapshot.data!.length,
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            itemBuilder: (context, index){
+              var webtoon = snapshot.data![index];
+              return WebToon(title: webtoon.title, thumb: webtoon.thumb, id: webtoon.id);
+            },
+            separatorBuilder: (context, index) =>
+            const SizedBox(width: 40,),
+          );
   }
 }
 
